17871fd663f8b63ace46d5345de52a6c9993cef4
thalesAlves758 <thalesalves758@gmail.com>
2023-04-18 22:07:27 -0300

    feat: create Product class

---
+++ src/main/java/Product.java
@@ -0,0 +1,25 @@
+public class Product {
+    private String name;
+    private PriceAdapter price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = new PriceAdapter(price);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price.getDecimalValue();
+    }
+
+    public void setPrice(double price) {
+        this.price.setDecimalValue(price);
+    }
+}
