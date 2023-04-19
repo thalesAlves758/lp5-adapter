5df727caa43a1d5c2459a8137cd49f42a7544702
thalesAlves758 <thalesalves758@gmail.com>
2023-04-18 22:07:37 -0300

    feat: create Product Repository class

---
+++ src/main/java/ProductRepository.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductRepository {
+    private List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+}
